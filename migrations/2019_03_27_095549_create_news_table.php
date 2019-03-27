49a59303940790919e8db8553e18479b19b14634
coffey <coffey@nligo.com>
2019-03-27 17:58:03 +0800

    no message

---
+++ migrations/2019_03_27_095549_create_news_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->nullable()->comment('标题');
+            $table->text('desc')->comment('描述');
+            $table->text('contents')->comment('内容');
+            $table->integer('sort')->comment('排序');
+            $table->dateTime();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
